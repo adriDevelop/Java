28ccf11b3fb5b12033a92e7e70e55feaae38cf0d
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2023-10-20 18:22:01 +0200

    Hola mundo: Manejo de variables.

--- Fundamentos/HolaMundo/src/HolaMundo.java
+++ Fundamentos/HolaMundo/src/HolaMundo.java
@@ -1,6 +1,24 @@
 // Mi clase en Java.
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo desde Java");
+        String saludar = "Hola mundo desde Java";
+        System.out.println(saludar);
+        // Haciendo uso de soutv.
+        System.out.println("saludar.toLowerCase() = " + saludar.toLowerCase());
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+        // Creamos nuevas variable.
+        int numero = 10;
+        boolean valor = true;
+        if (valor){
+            System.out.println("numero = " + numero);
+        }
+        // Asignamos tipo de dato implícitamente dado por el valor.
+        var numero3 = 15;
+
+        if ( numero > 10 ){
+            saludar = "Adrian";
+        }
+
+        System.out.println("saludar = " + saludar);
     }
 }
