d1bfdf8295026ba6e5ea6e540451e3f19f66456e
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2023-11-08 23:18:03 +0100

    Tipos de datos: Primitivos.

---
+++ Fundamentos/HolaMundo/src/PrimitivosFloat.java
@@ -0,0 +1,22 @@
+public class PrimitivosFloat {
+    public static void main(String[] args) {
+
+        float realFloat = 2e4f; //20000f
+        System.out.println("realFloat = " + realFloat);
+        System.out.println("float corresponde en byte a = " + Float.BYTES);
+        System.out.println("float corresponde en bites a = " + Float.SIZE);
+        System.out.println("Valor maximo para float = " + Float.MAX_VALUE);
+        System.out.println("Valor minimo para float = " + Float.MIN_VALUE);
+
+        double realDouble = 3.4028235E38;
+        System.out.println("realDouble = " + realDouble);
+        System.out.println("float corresponde en byte a = " + Double.BYTES);
+        System.out.println("float corresponde en bites a = " + Double.SIZE);
+        System.out.println("Valor maximo para float = " + Double.MAX_VALUE);
+        System.out.println("Valor minimo para float = " + Double.MIN_VALUE);
+
+
+
+
+    }
+}
