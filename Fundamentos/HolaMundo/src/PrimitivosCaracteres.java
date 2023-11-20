df1cf4243560adc83d8adbcc69a9805c6b50239f
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2023-11-20 21:59:03 +0100

    Primitivos Caracteres

---
+++ Fundamentos/HolaMundo/src/PrimitivosCaracteres.java
@@ -0,0 +1,22 @@
+public class PrimitivosCaracteres {
+    public static void main(String[] args) {
+        char caracter = '\u0040';
+        char decimal = 64;
+        System.out.println("caracter = " + caracter);
+        System.out.println("decimal = " + decimal);
+        System.out.println("decimal = caracter: " + (decimal == caracter));
+
+        char simbolo = '@';
+        System.out.println("simbolo = " + simbolo);
+        System.out.println("decimal = caracter: " + (simbolo == caracter));
+
+        char espacio = ' ';
+        char retroceso = '\b';
+        char tabulador = '\t';
+
+        System.out.println("char corresponde en byte:" + tabulador + Character.BYTES);
+        System.out.println("char corresponde en bites = " + Character.SIZE);
+        System.out.println("Character.MIN_VALUE = " + Character.MIN_VALUE);
+        System.out.println("Character.MAX_VALUE = " + Character.MAX_VALUE);
+    }
+}
