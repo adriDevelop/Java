51702a249cf2f7f7060e89d2aee5eb43ed83f71b
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2023-11-30 16:52:08 +0100

    Operadores lógicos ejemplo login.

--- Fundamentos/HolaMundo/src/OperadoresLogicosEjemploLogin.java
+++ Fundamentos/HolaMundo/src/OperadoresLogicosEjemploLogin.java
@@ -6,6 +6,8 @@ public class OperadoresLogicosEjemploLogin {
         // Definimos variables.
         String userName = "Adrian";
         String password = "12345";
+        String userName2 = "Andrea";
+        String password2 = "12345";
         Scanner sc = new Scanner(System.in);
 
         // Pedimos al cliente que introduzca los datos del login.
@@ -16,10 +18,10 @@ public class OperadoresLogicosEjemploLogin {
 
         // Comprobamos los datos que nos introduce el cliente para saber si accede o no.
         boolean esAutenticado = false;
-        esAutenticado = userName.equals(usuario) && password.equals(pass);
+        esAutenticado = (userName.equals(usuario) || userName2.equals(usuario)) && (password.equals(pass) || password2.equals(pass));
 
         if (esAutenticado)
-            System.out.println("Bienvenido ".concat(userName).concat("!"));
+            System.out.println("Bienvenido ".concat(usuario).concat("!"));
         else
             System.out.println("Datos introducidos no válidos.");
 
