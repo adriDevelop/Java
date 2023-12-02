8e748c5035063b6b0bddaef58557d3e8367fa3b0
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2023-12-02 20:25:55 +0100

    Sentencia for.

---
+++ Fundamentos/HolaMundo/src/SentenciaFor.java
@@ -0,0 +1,14 @@
+public class SentenciaFor {
+    public static void main(String[] args) {
+
+        // Sentencia for para incrementación.
+        for ( int i = 0; i < 10 ; i++){
+            System.out.println("i = " + i);
+        }
+
+        // Sentencia for para decrementación.
+        for ( int i = 10; i >= 0 ; i--){
+            System.out.println("i = " + i);
+        }
+    }
+}
