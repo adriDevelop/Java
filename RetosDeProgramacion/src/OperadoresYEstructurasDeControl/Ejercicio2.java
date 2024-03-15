d2c6b7ec67dd48a786b6750c1cd7d256111cd258
Adrian Velasco Carrasco <adridevelop11@gmail.com>
2024-03-15 20:21:23 +0100

    #01 Operadores y Estructuras de Control.

--- RetosDeProgramacion/src/OperadoresYEstructurasDeControl/Ejercicio2.java
+++ RetosDeProgramacion/src/OperadoresYEstructurasDeControl/Ejercicio2.java
@@ -1,18 +1,11 @@
 package OperadoresYEstructurasDeControl;
 
 public class Ejercicio2 {
-
-    /* DIFICULTAD EXTRA (opcional):
-     * Crea un programa que imprima por consola todos los números comprendidos
-     * entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
-     */
-
     public static void main(String[] args) {
 
-        for (int i = 10; i <= 50; i++){
-            if (i%2 == 0 && i != 16 && i%3 != 0)
-                System.out.println("Numero par, no es el 16 y tampoco es múltiplo de 3 = " + i);
+        for(int i = 10; i <= 50; i++){
+            if(i%2 == 0 && i != 16 && i%3 != 0)
+                System.out.println("i = " + i);
         }
     }
-
 }
